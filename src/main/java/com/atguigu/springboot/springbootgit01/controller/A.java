3c8ff076b989c92d64b3f23acd5665edfe0e000d
Layne <Layne@atguigu.com>
2022-06-16 15:10:38 +0800

    fist commit02

--- src/main/java/com/atguigu/springboot/springbootgit01/controller/A.java
+++ src/main/java/com/atguigu/springboot/springbootgit01/controller/A.java
@@ -7,4 +7,7 @@ package com.atguigu.springboot.springbootgit01.controller;
  * description:
  **/
 public class A {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
 }
