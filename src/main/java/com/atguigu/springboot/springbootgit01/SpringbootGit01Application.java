395c35aec2b3e584b413b9d67389c37609ff3e77
Layne <Layne@atguigu.com>
2022-06-16 15:08:40 +0800

    fist commit01

---
+++ src/main/java/com/atguigu/springboot/springbootgit01/SpringbootGit01Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot.springbootgit01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGit01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGit01Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/atguigu/springboot/springbootgit01/controller/A.java
@@ -0,0 +1,10 @@
+package com.atguigu.springboot.springbootgit01.controller;
+
+/**
+ * program: gittest
+ * author: 张爱斌
+ * create: 2022-06-16 15:02
+ * description:
+ **/
+public class A {
+}
