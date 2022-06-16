06a1c226897428efc2b6e8eb8c61c658b7e28fcb
Layne <Layne@atguigu.com>
2022-06-16 15:46:59 +0800

    master commit02

--- src/main/java/com/atguigu/springboot/springbootgit01/controller/A.java
+++ src/main/java/com/atguigu/springboot/springbootgit01/controller/A.java
@@ -10,6 +10,7 @@ public class A {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
         System.out.println("hello git!!!");
+        System.out.println("master test!");
         System.out.println("hot-fix commit01");
     }
 }
